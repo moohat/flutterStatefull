5b4746ce5de5bfa45e788d4542920ffd9601232b
moohat <refixzcorp@gmail.com>
2023-04-18 07:42:30 -0700

    Tab bar

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Text("Whatsapp"),
+          bottom: TabBar(tabs: [
+            Icon(Icons.camera_alt),
+            Text("Chats"),
+            Text("Status"),
+            Text("Calls"),
+          ]),
+        ),
+      ),
+    );
+  }
+}
