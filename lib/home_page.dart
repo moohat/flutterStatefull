d33320170379a011e7725c5270417724bb2e611c
moohat <refixzcorp@gmail.com>
2023-10-23 07:14:17 -0700

    responsive layout - MediaQuery

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,60 +6,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
+    Orientation orientation = MediaQuery.of(context).orientation;
 
     return Scaffold(
       backgroundColor: Colors.blueGrey,
-      body: Row(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            child: LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'MediaQuery: ${screenSize.width.toStringAsFixed(2)}',
-                      style: const TextStyle(color: Colors.white, fontSize: 18),
-                      textAlign: TextAlign.center,
-                    ),
-                    Text(
-                      'LayoutBuilder: ${constraints.maxWidth}',
-                      style: const TextStyle(color: Colors.white, fontSize: 18),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                );
-              },
-            ),
+          Text(
+            'Screen width: ${screenSize.width.toStringAsFixed(2)}',
+            style: const TextStyle(color: Colors.white, fontSize: 18),
+            textAlign: TextAlign.center,
           ),
-          Expanded(
-            flex: 3,
-            child: LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-                return Container(
-                  color: Colors.white,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        'MediaQuery: ${screenSize.width.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                            color: Colors.blueGrey, fontSize: 18),
-                        textAlign: TextAlign.center,
-                      ),
-                      Text(
-                        'LayoutBuilder: ${constraints.maxWidth}',
-                        style: const TextStyle(
-                            color: Colors.blueGrey, fontSize: 18),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
+          Text(
+            'Orientation: $orientation',
+            style: const TextStyle(color: Colors.white, fontSize: 18),
+            textAlign: TextAlign.center,
           ),
         ],
       ),
